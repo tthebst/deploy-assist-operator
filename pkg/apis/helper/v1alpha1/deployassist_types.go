980de5ba9d53d9a461befbc85d2b44360992204c
tthebst <gretler.tim@gmail.com>
2020-07-10 16:06:13 +0200

    update crd

--- pkg/apis/helper/v1alpha1/deployassist_types.go
+++ pkg/apis/helper/v1alpha1/deployassist_types.go
@@ -12,6 +12,7 @@ type DeployassistSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Files []string `json:"files"`
 }
 
 // DeployassistStatus defines the observed state of Deployassist
@@ -19,6 +20,7 @@ type DeployassistStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	AppliedFiles []string `json:"applied_files"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
